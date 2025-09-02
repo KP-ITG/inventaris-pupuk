d136f1d09032016a845dbe826503624d5737db47
romiyusnandar <yusromi04@gmail.com>
2025-09-02 10:54:13 +0700

    Refactor home route to redirect authenticated users to the dashboard and unauthenticated users to the login page
    Signed-off-by: romiyusnandar <yusromi04@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,15 @@
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    if (Auth::check()) {
+        return redirect()->route('dashboard');
+    }
+    return redirect()->route('login');
 });
 
 Route::get('/dashboard', [DashboardController::class, 'index'])
